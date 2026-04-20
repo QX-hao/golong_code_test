e216f028d9c87ca16cefed1755034a5d8104c833
QX-hao <1039757986@qq.com>
2026-04-20 20:28:02 +0800

    test push

--- some-test/TestDemo1/main.go
+++ some-test/TestDemo1/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"TestDemo1/server"
 	"fmt"
-	// "log"
+	"log"
 )
 
 
 func main() {
+	log.Println("hello world")
 
 	config,err := server.InitConfig_v1("config.yaml")
 	if err != nil {
